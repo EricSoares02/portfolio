00e3037343faf1755888c7bbb2a8d8466458866a
Eric Soares <ericsoareslopesdev@gmail.com>
2024-07-01 16:12:46 -0300

    feat: add animation hook

---
+++ src/hooks/animation/defaultAnimationsKey.ts
@@ -0,0 +1,8 @@
+
+
+export enum AnimationKey{
+    "SlideIn",
+    "Bounce",
+    "SlideOut",
+    
+}
---
+++ src/hooks/animation/setAnimations.ts
@@ -0,0 +1,50 @@
+import { useEffect } from "react";
+import { AnimationKey } from "./defaultAnimationsKey";
+
+
+const Observer = (animationClass: string) =>{
+
+    return new IntersectionObserver((entries)=>{
+        entries.forEach((entry)=>{
+            if (entry.isIntersecting) {
+                entry.target.classList.add(animationClass)
+            } else {
+                entry.target.classList.remove(animationClass)
+            }
+        })
+    });
+}
+
+
+const SetAnimation = (elementClass: string, animationClass: string) =>{
+
+    useEffect(() => {
+        const element = document.querySelector(`.${elementClass}`)
+        Observer(animationClass).observe(element)
+    });
+
+}
+
+
+
+const useCssAnimations = (elements: Array<{elementClass: string, 
+    animationKey: AnimationKey | string}>) => {
+
+    elements.forEach((n)=>{
+        switch (n.animationKey) {
+            case AnimationKey.Bounce:
+                return SetAnimation(n.elementClass, "bounce")
+
+            case AnimationKey.SlideIn:
+                return SetAnimation(n.elementClass, "slideIn")
+
+            case AnimationKey.SlideOut:
+                return SetAnimation(n.elementClass, "slideOut")
+                                
+            default:
+                return SetAnimation(n.elementClass, n.animationKey)
+        }
+    })
+}
+
+export default useCssAnimations;
